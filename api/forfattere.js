74c3f6b2bd6f5b95a000de4aa54e60db6ab0c6d7
gloer <gloer@langslet.com>
2020-04-21 11:43:00 +0200

    Changed text

--- api/forfattere.js
+++ api/forfattere.js
@@ -3,18 +3,22 @@ let forfattere = []
 
 let f = db.collection("forfattere")
 
-f.onSnapshot(snap => {
-    forfattere = snap.docs
-    console.log(forfattere)
-})
+
 
 
 const createApi = async () => {
 
-    module.exports = (req, res) => {
-        const { name = 'World' } = req.query
-        res.status(200).send(`HEI ${name}`)
-    }
+    f.onSnapshot(snap => {
+        forfattere = snap.docs
+        console.log(forfattere)
+    }).then(() => {
+        module.exports = (req, res) => {
+            const { name = 'BOB' } = req.query
+            res.status(200).send(`HEI ${name}`)
+        }
+    })
+
+    
 
 }
 
