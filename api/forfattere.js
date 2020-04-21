665396b8f45559839e3420ecec6dc7317af5fcb0
gloer <gloer@langslet.com>
2020-04-21 11:38:44 +0200

    Aagin

--- api/forfattere.js
+++ api/forfattere.js
@@ -6,10 +6,9 @@ let f = db.collection("forfattere")
 f.onSnapshot(snap => {
     forfattere = snap.docs
     console.log(forfattere)
-    
-    module.exports = (req, res) => {
-        const { name = 'World' } = req.query
-        res.status(200).send(`HEI ${name}`)
-    }
-
 })
+
+module.exports = (req, res) => {
+    const { name = 'World' } = req.query
+    res.status(200).send(`HEI ${name}`)
+}
