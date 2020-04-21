d334212fb105b8859c9b1df996730eec81c4d5ae
gloer <gloer@langslet.com>
2020-04-21 14:04:36 +0200

    removed cfirebase

--- api/forfattere.js
+++ api/forfattere.js
@@ -1,31 +1,7 @@
-import { db } from "./../src/firebase.js"
-let forfattere = []
-
-let f = db.collection("forfattere")
-
-
-
-
-const createApi = () => {
-
-    module.exports = (req, res) => {        
-        res.status(100).send(`VENTER`)
-    }
-
-    f.onSnapshot(snap => {
-        forfattere = snap.docs
-        console.log(forfattere)
-    }).then(() => {
-        module.exports = (req, res) => {
-            const { name = 'BOB' } = req.query
-            res.status(200).send(`HEI ${name}`)
-        }
-    })
-
-    
-
+   
+module.exports = (req, res) => {
+    const { name = 'BOB' } = req.query
+    res.status(200).send(`HEI ${name}`)
 }
-
-createApi()
-
+    
 
